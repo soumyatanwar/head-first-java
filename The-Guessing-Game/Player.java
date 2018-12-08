e9672d328c5b5c2cd3155f97abdd3908f9371c58
soumyatanwar <37302163+soumyatanwar@users.noreply.github.com>
2018-12-08 21:00:43 +0530

    Create Player.java

---
+++ The-Guessing-Game/Player.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+Scanner sc = new Scanner(System.in);
+public class Player
+{
+  numberGuessed;
+  void guess()
+  {
+    System.out.println("Make a guess");
+    numberGuessed = sc.nextInt();
+  }
+}
